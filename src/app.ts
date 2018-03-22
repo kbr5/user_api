8f2bb42bdda57c202ee862a86cf75cd7aecbcda5
kbr5 <35654137+kbr5@users.noreply.github.com>
2018-03-22 12:05:45 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ class App {
       const router = express.Router()
       router.get('/', (req, res) => {
         res.json({
-          message: 'Hello HX!'
+          message: 'Hello!'
         })
       })
       // all
